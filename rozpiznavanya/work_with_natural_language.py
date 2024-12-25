5a0df6c53b5966f926b29bf448058dd926940bbe
Illia452 <dvgillia@gmail.com>
2024-12-25 12:20:07 +0200

    added folder "models" and ".gitignore"

--- rozpiznavanya/work_with_natural_language.py
+++ rozpiznavanya/work_with_natural_language.py
@@ -1,12 +1,6 @@
 # class TransformationLanguage():
 #     pass
 import re
-import spacy
-
-# nlp = spacy.load("uk_core_news_sm")
-# doc = nlp("Привіт, включи музику, а також ще щось неламане\n\n")
-# for token in doc:
-#     print(token.text)
 
 
 nlpp = ("Привіт!!! включи-музику, а також ще щось неламане &2,?")
@@ -18,16 +12,3 @@ for i in nlpp:
 print(token_text)
 
     
-
-
-print(nlpp)
-
-
-
-
-# l = ["1", "1", "1", "2", "1", "1"]
-# filtered = []
-# for i in l:
-#     if i == "1":
-#         filtered.append(i)
-# print(filtered)
