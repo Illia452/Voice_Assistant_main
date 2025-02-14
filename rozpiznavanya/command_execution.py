ac6d7036196da3232b06a326fccd44e7ac1b7dc7
Illia452 <dvgillia@gmail.com>
2025-02-14 19:45:52 +0200

    Added mini-analyze command

--- rozpiznavanya/command_execution.py
+++ rozpiznavanya/command_execution.py
@@ -8,8 +8,8 @@ class MakeCommands():
         self.output_data=[]
         with open('synonyms.json', 'r', encoding='utf-8') as f:
             self.output_data = json.load(f)
-        self.list_com = []
-        self.com = []
+        self.list_command = []
+        self.command = []
 
     def analyze_command(self, text):
         for i in text:
@@ -19,15 +19,25 @@ class MakeCommands():
 
         for key, vallue in self.output_data.items():
             for values in vallue:
-                if values in self.main_text[0]:
-                    print(f"НАША КОМАНДА {values} з ключа {key}")
-                    if key == "scrin":
-                        self.voice_commands.TakeScreenShot()
-                    elif key == "open_browser":
-                        self.voice_commands.OpenBrowser()
+                for text_num in self.main_text:
+                    if values in text_num:
+                        self.command.append(len(self.main_text))
+                        self.command.append(key)
+                        self.list_command.append(self.command)
+
+                        self.command = []
+                        
+        print(self.list_command)
+
+                        # print(f"НАША КОМАНДА {values} з ключа {key}")
+                        # if key == "scrin":
+                        #     self.voice_commands.TakeScreenShot()
+                        # elif key == "open_browser":
+                        #     self.voice_commands.OpenBrowser()
                     
                     
                     # elif key == "select_file" and (self.time_make_command_browze == 0.0 or time.time() - self.time_make_command_browze > 2):
                     #     self.voice_commands.SelectFile()
                     #     self.time_make_command_selectfile = time.time()
         self.main_text = []
+        self.list_command = []
