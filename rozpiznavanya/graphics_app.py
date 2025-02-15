c96a856f698373cb463f55ed7db7315703e86078
Illia452 <dvgillia@gmail.com>
2025-02-16 00:57:34 +0200

    added first designer app

---
+++ rozpiznavanya/graphics_app.py
@@ -0,0 +1,20 @@
+from PyQt5 import QtWidgets
+from PyQt5.QtWidgets import QApplication, QMainWindow
+import sys
+
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("Widgets App")
+        self.setGeometry(500,150,960,680)
+
+def main():
+    app = QApplication(sys.argv)
+    window = MainWindow()
+    window.show()
+    sys.exit(app.exec_())
+    
+
+if __name__ == "__main__":
+    main()
